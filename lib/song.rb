aabeea94ec97939b2d01c16ec12dfe71d05b829b
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-13 05:27:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   
   attr_accessor :name
